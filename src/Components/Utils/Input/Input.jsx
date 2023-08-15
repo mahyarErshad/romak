8dfd8490b200983d7ae09e700698fb54194ab44c
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 16:36:57 +0330

    Cleaned the code

--- src/Components/Utils/Input/Input.jsx
+++ src/Components/Utils/Input/Input.jsx
@@ -3,16 +3,17 @@ import React, { useState } from "react";
 function Input({ label, type = "text", setter, value }) {
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const border = error ? "border-red-400" : "border-blue-400 hover:border-blue-500 focus-within:border-blue-600";
+
   function handleBlur() {
-    if (!value.trim().length) {
-      setError(true);
-      setErrorMessage(`${label} نمیتواند خالی باشد!`);
-    } else {
-      setError(false);
-      setErrorMessage("");
-    }
+    const trimmedValue = value.trim();
+    const hasError = !trimmedValue.length;
+
+    setError(hasError);
+    setErrorMessage(hasError ? `${label} نمیتواند خالی باشد!` : "");
   }
+
+  const border = error ? "border-red-400" : "border-blue-400 hover:border-blue-500 focus-within:border-blue-600";
+
   return (
     <div className="flex-col gap-2">
       <label className="cursor-pointer" htmlFor={label}>
