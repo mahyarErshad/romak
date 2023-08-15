c6a8d88180c99655caca962a7a8ba8339769a6aa
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 10:58:43 +0330

    Created LoginPage skeleton

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { ReactComponent as ReactLogo } from "../../assets/images/logo.svg";
 
 function LoginPage() {
-  return <section className="w-full h-full bg-red-500 grid max-md:grid-cols-1 grid-cols-2"></section>;
+  return (
+    <section className="w-full h-full p-8 grid max-md:grid-cols-1 grid-cols-2">
+      <div className="flex-center max-md:border-l-0 border-l-2 border-gray-200">
+        <ReactLogo />
+      </div>
+      <div className="flex-center">form</div>
+    </section>
+  );
 }
 
 export default LoginPage;
