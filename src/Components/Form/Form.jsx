16cf7c4e599660334f67ed16323bcc024c12af48
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 12:45:54 +0330

    Form validation is done

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,6 +4,8 @@ import Input from "../Utils/Input/Input";
 function Form() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const formData = [
     {
@@ -21,11 +23,29 @@ function Form() {
     },
   ];
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!userName.length || !password.length) {
+      setError(true);
+      setErrorMessage("فیلدها نباید خالی باشند!");
+      return;
+    }
+    if (userName !== "kminchelle" || password !== "0lelplR") {
+      setError(true);
+      setErrorMessage("نام کاربری یا کلمه عبور اشتباه است!");
+    } else {
+      setError(false);
+    }
+  }
+
   return (
-    <form className="flex-col gap-4">
+    <form onSubmit={(e) => handleSubmit(e)} className="flex-col gap-4">
       {formData.map((item) => (
         <Input key={item.id} label={item.label} value={item.value} setter={item.setter} type={item.type} />
       ))}
+      <button className="bg-blue-600 text-white font-bold px-4 py-3 rounded-md hover:opacity-90 duration-300 mt-4">ورود به حساب کاربری</button>
+      <p className="text-sm text-blue-600 cursor-pointer">رمز عبورم را فراموش کردم!</p>
+      {error && <p className="text-sm text-red-500">{errorMessage}</p>}
     </form>
   );
 }
