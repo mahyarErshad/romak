0832b0c8d103c45e1cc1b5be5c51fd33c05b6e53
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 13:03:50 +0330

    Getting token and saving it in local storage is done

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -6,6 +6,7 @@ function Form() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const formData = [
     {
@@ -23,7 +24,7 @@ function Form() {
     },
   ];
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     if (!userName.length || !password.length) {
       setError(true);
@@ -34,7 +35,26 @@ function Form() {
       setError(true);
       setErrorMessage("نام کاربری یا کلمه عبور اشتباه است!");
     } else {
+      setLoading(true);
       setError(false);
+      try {
+        const response = await fetch("https://dummyjson.com/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: userName,
+            password: password,
+          }),
+        });
+        const data = await response.json();
+        console.log(data);
+        localStorage.setItem("token", data.token);
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
     }
   }
 
@@ -43,7 +63,9 @@ function Form() {
       {formData.map((item) => (
         <Input key={item.id} label={item.label} value={item.value} setter={item.setter} type={item.type} />
       ))}
-      <button className="bg-blue-600 text-white font-bold px-4 py-3 rounded-md hover:opacity-90 duration-300 mt-4">ورود به حساب کاربری</button>
+      <button disabled={loading} className="bg-blue-600 text-white font-bold px-4 py-3 rounded-md hover:opacity-90 duration-300 mt-4 disabled:opacity-60">
+        {loading ? "در حال ارسال..." : "ورود به حساب کاربری"}
+      </button>
       <p className="text-sm text-blue-600 cursor-pointer">رمز عبورم را فراموش کردم!</p>
       {error && <p className="text-sm text-red-500">{errorMessage}</p>}
     </form>
