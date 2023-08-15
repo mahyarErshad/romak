2dedd889ef15e1fb5e564201a1dc31dfd6e49e08
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 14:49:07 +0330

    Products page is done

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../../Components/Utils/Loading/Loading";
 import { useNavigate } from "react-router-dom";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
@@ -8,7 +9,7 @@ function Products() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const apiUrl = "https://dummyjson.com/auth/products";
-
+  const [loading, setLoading] = useState(false);
   const [rowData, setRowData] = useState([]);
 
   useEffect(() => {
@@ -17,9 +18,10 @@ function Products() {
     } else {
       fetchData();
     } // eslint-disable-next-line
-  }, [navigate, token]);
+  }, [token]);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
       const response = await fetch(apiUrl, {
         method: "GET",
@@ -37,17 +39,29 @@ function Products() {
     } catch (error) {
       console.error(error);
     }
+    setLoading(false);
   };
 
   const columnDefs = [
-    { headerName: "ID", field: "id" },
-    { headerName: "Title", field: "title" },
-    { headerName: "Price", field: "price" },
+    { headerName: "ID", field: "id", floatingFilter: true, filter: true },
+    {
+      headerName: "Title",
+      field: "title",
+      floatingFilter: true,
+      filter: true,
+    },
+    {
+      headerName: "Price",
+      field: "price",
+      floatingFilter: true,
+      filter: true,
+    },
   ];
 
+  if (loading) return <Loading />;
   return (
     <section className="w-full h-full p-4 flex justify-center">
-      <div className="ag-theme-alpine" style={{ height: "auto", width: 600 }}>
+      <div className="ag-theme-alpine" style={{ width: 600 }}>
         <AgGridReact rowData={rowData} columnDefs={columnDefs} domLayout="autoHeight" />
       </div>
     </section>
