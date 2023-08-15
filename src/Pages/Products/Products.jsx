970781887dda961de1fc8e1846253844718d2e55
mahyarErshad <mahyar.ershad92@gmail.com>
2023-08-15 14:08:34 +0330

    Created table using agGrid

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 function Products() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const apiUrl = "https://dummyjson.com/auth/products";
+
+  const [rowData, setRowData] = useState([]);
 
   useEffect(() => {
     if (!token) {
@@ -12,7 +17,7 @@ function Products() {
     } else {
       fetchData();
     } // eslint-disable-next-line
-  }, [token]);
+  }, [navigate, token]);
 
   const fetchData = async () => {
     try {
@@ -22,14 +27,31 @@ function Products() {
           Authorization: `Bearer ${token}`,
         },
       });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
       const data = await response.json();
-      console.log(data.products);
+      setRowData(data.products);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return <div>Products</div>;
+  const columnDefs = [
+    { headerName: "ID", field: "id" },
+    { headerName: "Title", field: "title" },
+    { headerName: "Price", field: "price" },
+  ];
+
+  return (
+    <section className="w-full h-full p-4 flex justify-center">
+      <div className="ag-theme-alpine" style={{ height: "auto", width: 600 }}>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs} domLayout="autoHeight" />
+      </div>
+    </section>
+  );
 }
 
 export default Products;
